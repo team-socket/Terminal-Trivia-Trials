86e6bc829366d07d819358beb3269d7df6dc0f60
Brandon Perard <Brandon.Perard@Gmail.com>
2023-03-15 23:57:31 -0700

    Updated socket to deployed main

--- socket.js
+++ socket.js
@@ -3,12 +3,12 @@
 const { io } = require('socket.io-client');
 
 // FOR LOCAL DEPLOY
-const socket = io('http://localhost:3001/');
+// const socket = io('http://localhost:3001/');
 
 // DEPLOYED DEV SOCKET SERVER
 // const socket = io('https://team-socket-server-dev.onrender.com');
 
 // DEPLOYED MAIN SOCKET SERVER
-// const socket = io('https://team-socket-server.onrender.com');
+const socket = io('https://team-socket-server.onrender.com');
 
 module.exports = { socket };
